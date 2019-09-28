623097b0dbec79f30ae41626f7ccf413226a0d32
BYRDZ <jonathanngdixon@gmail.com>
2019-09-28 14:02:59 -0500

    Initial commit

---
+++ app/src/main/java/io/github/pioneerdevelopers/cal/MainActivity.java
@@ -0,0 +1,14 @@
+package io.github.pioneerdevelopers.cal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
